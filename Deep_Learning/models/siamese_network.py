dd0938068ed48c32411d0c8913aec672f996d7ce
immuno121 <shasvat.desai@gmail.com>
2019-12-09 01:34:20 -0500

    added code for embedding net, siamese net and triplet net

---
+++ Deep_Learning/models/siamese_network.py
@@ -0,0 +1,118 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class EmbeddingNet(nn.Module):
+    def __init__(self, num_classes):
+        super(EmbeddingNet, self).__init__()
+        self.num_classes = num_classes
+        '''
+        self.convnet = nn.Sequential(nn.Conv2d(1, 32, 5), nn.PReLU(),
+                                     nn.MaxPool2d(2, stride=2),
+                                     nn.Conv2d(32, 64, 5), nn.PReLU(),
+                                     nn.MaxPool2d(2, stride=2))
+
+        self.fc = nn.Sequential(nn.Linear(64 * 4 * 4, 256),
+                                nn.PReLU(),
+                                nn.Linear(256, 256),
+                                nn.PReLU(),
+                                nn.Linear(256, 2)
+                                )
+        '''
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(4, 16, kernel_size=(3,7), stride=1),#Nxchxw
+            nn.PReLU(),
+            nn.MaxPool2d(kernel_size=(3,7)))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=3, stride=1),
+            nn.PReLU(),
+            nn.MaxPool2d(kernel_size=2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=1),
+            nn.PReLU(),
+            nn.MaxPool2d(kernel_size=2))
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1),
+            nn.PReLU(),
+            nn.MaxPool2d(kernel_size=2))
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1),
+            nn.PReLU(),
+            nn.MaxPool2d(kernel_size=2))
+
+
+        #self.fc = nn.Linear(256*132*41, num_classes) #Mel
+        self.fc = nn.Linear(256*133*41, self.num_classes)
+
+    def forward(self, x):
+        '''
+        output = self.convnet(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc(output)
+        return output
+        '''
+        out = self.layer1(x)
+        #print(out.size())
+        out = self.layer2(out)
+        #print(out.size())
+        out = self.layer3(out)
+        #print(out.size())
+        out = self.layer4(out)
+        #print(out.size())
+        out = self.layer5(out)
+        #print(out.size())
+        out = out.reshape(out.size(0), -1)
+        #print(out.size())
+        
+        out = self.fc(out)
+        return out
+
+    def get_embedding(self, x):
+        return self.forward(x)
+
+
+class ClassificationNet(nn.Module):
+    def __init__(self, embedding_net, n_classes):
+        super(ClassificationNet, self).__init__()
+        self.embedding_net = embedding_net
+        self.n_classes = n_classes
+        self.nonlinear = nn.PReLU()
+        self.fc1 = nn.Linear(2, n_classes)
+
+    def forward(self, x):
+        output = self.embedding_net(x)
+        output = self.nonlinear(output)
+        scores = F.log_softmax(self.fc1(output), dim=-1)
+        return scores
+
+    def get_embedding(self, x):
+        return self.nonlinear(self.embedding_net(x))
+
+
+class SiameseNet(nn.Module):
+    def __init__(self, embedding_net):
+        super(SiameseNet, self).__init__()
+        self.embedding_net = embedding_net
+
+    def forward(self, x1, x2):
+        output1 = self.embedding_net(x1)
+        output2 = self.embedding_net(x2)
+        return output1, output2
+
+    def get_embedding(self, x):
+        return self.embedding_net(x)
+
+
+class TripletNet(nn.Module):
+    def __init__(self, embedding_net):
+        super(TripletNet, self).__init__()
+        self.embedding_net = embedding_net
+
+    def forward(self, x1, x2, x3):
+        output1 = self.embedding_net(x1)
+        output2 = self.embedding_net(x2)
+        output3 = self.embedding_net(x3)
+        return output1, output2, output3
+
+    def get_embedding(self, x):
+        return self.embedding_net(x)
